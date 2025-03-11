8222380c01c2cda5b3ab217c6c6667395986e8f1
keh38 <Ken_Hancock@meei.harvard.edu>
2025-03-11 15:55:33 -0400

    adding networking

---
+++ HTS Controller/MainForm.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace TabletInterface
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.connectionStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.imageList = new System.Windows.Forms.ImageList(this.components);
+            this.statusStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.connectionStatusLabel});
+            this.statusStrip.Location = new System.Drawing.Point(0, 428);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(800, 22);
+            this.statusStrip.TabIndex = 0;
+            this.statusStrip.Text = "statusStrip1";
+            // 
+            // connectionStatusLabel
+            // 
+            this.connectionStatusLabel.Name = "connectionStatusLabel";
+            this.connectionStatusLabel.Size = new System.Drawing.Size(86, 17);
+            this.connectionStatusLabel.Text = "Not connected";
+            // 
+            // imageList
+            // 
+            this.imageList.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("imageList.ImageStream")));
+            this.imageList.TransparentColor = System.Drawing.Color.Transparent;
+            this.imageList.Images.SetKeyName(0, "nav_plain_red.png");
+            this.imageList.Images.SetKeyName(1, "nav_plain_green.png");
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.statusStrip);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Human Test Suite Controller";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.Shown += new System.EventHandler(this.MainForm_Shown);
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel connectionStatusLabel;
+        private System.Windows.Forms.ImageList imageList;
+    }
+}
+
---
+++ HTS Controller/MainForm.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Reflection;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using Serilog;
+using SerilogTraceListener;
+
+using KLib.Net;
+
+namespace TabletInterface
+{
+    public partial class MainForm : Form
+    {
+        private IPEndPoint _ipEndPoint;
+        private string _serverAddress;
+
+        private CancellationTokenSource _serverCancellationToken;
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private async Task StartLogging()
+        {
+            await Task.Run(() =>
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Verbose()
+                .WriteTo.Console()
+                .WriteTo.File(
+                    Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), "EPL", "Logs", "HTSController-.txt"),
+                    rollingInterval: RollingInterval.Day,
+                    retainedFileCountLimit: 30,
+                    buffered: true)
+                .CreateLogger()
+                );
+
+            var listener = new SerilogTraceListener.SerilogTraceListener();
+            Trace.Listeners.Add(listener);
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void MainForm_Shown(object sender, EventArgs e)
+        {
+            await StartLogging();
+
+            Log.Information($"HTS  v{Assembly.GetExecutingAssembly().GetName().Version.ToString()} started");
+
+            Log.Information("Starting TCP listener");
+            StartListener();
+
+            await InitializeTabletConnection();
+        }
+
+        private async Task InitializeTabletConnection()
+        {
+            await SearchForConnection();
+
+            if (_ipEndPoint != null)
+            {
+                //await Task.Run(() => GetSubjectInfo());
+                //await Task.Run(() => GetTabletConfiguration());
+            }
+        }
+
+        private async Task SearchForConnection()
+        {
+            await Task.Run(() => Discover());
+
+            if (_ipEndPoint == null)
+            {
+                connectionStatusLabel.Image = imageList.Images[0];
+                connectionStatusLabel.Text = "No tablet connection (double-click to retry)";
+            }
+            else
+            {
+                connectionStatusLabel.Image = imageList.Images[1];
+                connectionStatusLabel.Text = $"Connected to {_ipEndPoint.ToString()}";
+            }
+        }
+
+        private bool Discover()
+        {
+            bool success = false;
+
+            connectionStatusLabel.Image = imageList.Images[0];
+            connectionStatusLabel.Text = "Connecting to tablet...";
+            Log.Information("Connecting to tablet");
+
+            try
+            {
+                _ipEndPoint = Discovery.Discover("HEARING.TEST.SUITE");
+                if (_ipEndPoint != null)
+                {
+                    Debug.WriteLine($"pinging host {_ipEndPoint.ToString()}");
+
+                    var result = KTcpClient.SendMessage(_ipEndPoint, $"Ping:{_serverAddress}");
+                    Log.Information($"Ping: {(result > 0).ToString()}");
+                    success = (result > 0);
+                }
+
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.Message); 
+                success = false;
+                _ipEndPoint = null; ;
+            }
+
+            return success;
+        }
+
+        private void StartListener()
+        {
+            _serverCancellationToken = new CancellationTokenSource();
+            Task.Run(() =>
+            {
+                Listener(_serverCancellationToken.Token);
+            }, _serverCancellationToken.Token);
+        }
+
+        private void Listener(CancellationToken ct)
+        {
+            var server = new KTcpListener();
+            server.StartListener(4951);
+
+            _serverAddress = server.ListeningOn;
+            Debug.WriteLine($"TCP server started on {server.ListeningOn}");
+
+            while (!ct.IsCancellationRequested)
+            {
+                try
+                {
+                    if (server.Pending())
+                    {
+                        ProcessTCPMessage(server);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.Message);
+                }
+            }
+
+            server.CloseListener();
+            Debug.WriteLine("TCP server stopped");
+        }
+
+        private void ProcessTCPMessage(KTcpListener server)
+        {
+            server.AcceptTcpClient();
+
+            //UserResponse response = null;
+            //ErrorDescription error = null;
+
+            //string input = server.ReadString();
+            //switch (input)
+            //{
+            //    case "error":
+            //        var bytes = server.ReadBytes();
+            //        error = SRI.Messages.Message.FromProtoBuf<ErrorDescription>(bytes);
+            //        break;
+
+            //    case "item":
+            //        bytes = server.ReadBytes();
+            //        _currentItem = SRI.Messages.Message.FromProtoBuf<CurrentItem>(bytes);
+            //        break;
+
+            //    case "response":
+            //        bytes = server.ReadBytes();
+            //        response = SRI.Messages.Message.FromProtoBuf<UserResponse>(bytes);
+            //        break;
+            //}
+            //server.CloseTcpClient();
+
+            //switch (input)
+            //{
+            //    case "play finished":
+            //        _isPlaying = false;
+            //        break;
+
+            //    case "error":
+            //        _isPlaying = false;
+            //        this.Invoke(this.processErrorDelgate, error);
+            //        break;
+
+            //    case "item":
+            //        this.Invoke(this.showCurrentItemDelegate);
+            //        break;
+
+            //    case "response":
+            //        this.Invoke(this.processResponseDelgate, response);
+            //        break;
+            //}
+        }
+
+    }
+}
