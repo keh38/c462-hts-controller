1e2dfec634972c9089ff8ddfc5c420b6d1f40147
keh38 <Ken_Hancock@meei.harvard.edu>
2026-05-19 16:47:24 -0400

    testing gamebridge tweaks

--- HTS Controller/Properties/AssemblyInfo.cs
+++ HTS Controller/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("HTSController")]
-[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
